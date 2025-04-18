77a1465e91c395cdde46884672b96fbaa1b352a3
WhaleBux <34806168+iShinzo@users.noreply.github.com>
2025-04-18 19:50:10 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all routes
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data:;
+              connect-src 'self' https://whalebux-vercel.onrender.com;
+              font-src 'self';
+              object-src 'none';
+            `.replace(/\s{2,}/g, ' ').trim(), // Minify the CSP string
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
