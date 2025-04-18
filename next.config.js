3cda81e8959541b0c5775e8c4a2ff09199086833
WhaleBux <34806168+iShinzo@users.noreply.github.com>
2025-04-18 19:56:55 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self';
+              script-src 'self' 'unsafe-inline';
               style-src 'self' 'unsafe-inline';
               img-src 'self' data:;
               connect-src 'self' https://whalebux-vercel.onrender.com;
