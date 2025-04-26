d5759809a76b4152fd656d3d79ad248db08e3dba
iShinzo <madars.dzenis@gmail.com>
2025-04-26 18:31:50 +0300

    fix: correct script tag syntax in _document.js using next/script

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -7,7 +8,7 @@ export default function Document() {
         {/* Add favicon */}
         <link rel="icon" type="image/png" href="/placeholder-logo.png" />
         {/* Telegram WebApp script */}
-        <script src="https://telegram.org/js/telegram-web-app.js" />
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
         {/* Vercel Live Feedback script allow (optional for dev) */}
         {/* <script src="https://vercel.live/_next-live/feedback/feedback.js" /> */}
       </Head>
