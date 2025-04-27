870cc32c6ead8fcb75b53718eb6e05d74e2d0544
iShinzo <madars.dzenis@gmail.com>
2025-04-27 20:29:33 +0300

    Add minimal index page to test Telegram WebApp context integration

--- app/_app.tsx
+++ app/_app.tsx
@@ -2,22 +2,6 @@ import { useTelegramWebApp } from "../hooks/useTelegramWebApp";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: { Component: any; pageProps: any }) {
-  const tg = useTelegramWebApp();
-
-  return (
-    <>
-      {/* Telegram Debug Info (remove or hide in production) */}
-      {!tg ? (
-        <div style={{ color: 'red', padding: 8 }}>Not running inside Telegram WebApp.</div>
-      ) : (
-        <div style={{ color: 'green', padding: 8 }}>
-          Telegram Context Detected!<br />
-          <pre style={{ color: 'inherit', background: '#222', padding: 8, borderRadius: 6 }}>
-            {JSON.stringify(tg.initDataUnsafe?.user, null, 2)}
-          </pre>
-        </div>
-      )}
-      <Component {...pageProps} />
-    </>
-  );
+  useTelegramWebApp(); // Still initialize the hook for context
+  return <Component {...pageProps} />;
 }
